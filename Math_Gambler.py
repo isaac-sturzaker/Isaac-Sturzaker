13872c6c8922230a5c965c143d7f7bf11b357778
isaac-sturzaker <52386759+isaac-sturzaker@users.noreply.github.com>
2019-07-23 14:38:29 +1200

    Count Down
    I have made a count down which will be used when starting the game mode

--- Math_Gambler.py
+++ Math_Gambler.py
@@ -2,8 +2,24 @@
 #Author: Isaac Sturzaker
 #Date Started: 23/07/19
 
+def classic_func():
+    """Classic Mode is a certan amount of questions
+       and each correct answer gives you a set amount of points
+    """
 
+    
 
+    #Imports the time library
+    import time 
+
+    #A countdown to start the mode
+    num = 3 
+    for i in range(3):
+        print("{}...".format(num))
+        num -= 1
+        for i in range(1):
+            time.sleep(1)
+            
 
 def main():
     """This is the main funtion which is going to
@@ -14,7 +30,7 @@ def main():
     repeat = True #Repeat question untill valid answer is input
     while True:
         #Asking the user which mode they would like to play
-        game_mode = input("\nPlease choose a game mode\n (C)Classic\n (A)Arcade\n (H)Help \n").lower().strip()
+        game_mode = input("\nPlease choose a game mode\n (C)Classic\Panui\n (A)Arcade\n (H)Help\Awhina \n").lower().strip()
 
         #If statement that takes user to function dependet on the mode they chose
         if game_mode == "c":
